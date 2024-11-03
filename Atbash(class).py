fb9a571cda7855de7eb274434bd163767797c46a
5-Olga-666 <gravit.ov@phystech.edu>
2024-11-03 23:28:48 +0300

    Add files via upload

---
+++ Atbash(class).py
@@ -0,0 +1,18 @@
+class Atbash:#создание класса
+	    alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"#присвоение переменной alphabet значения "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+
+	    def __init__(self):#ключевой метод класса _init_
+	        lowercase_code = {x: y for x, y in zip(self.alphabet, self.alphabet[::-1])}
+	        uppercase_code = {x.upper(): y.upper() for x, y in zip(self.alphabet, self.alphabet[::-1])}
+	        self._encode = lowercase_code
+	        self._encode.update(uppercase_code)
+
+	    def encode(self, text):
+	        return ''.join([self._encode.get(char, char) for char in text])
+
+
+cipher = Atbash()
+line = input()
+while line != '.':
+    print(cipher.encode(line))
+    line = input()
