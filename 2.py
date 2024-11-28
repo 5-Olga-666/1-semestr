10bf7f6b287f39fa566c0b9752bdc842c392897c
5-Olga-666 <gravit.ov@phystech.edu>
2024-11-28 23:57:08 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, data):
+        self.item = data
+        self.nref = None
+        self.pref = None
+class DoublyLinkedList:
+    def __init__(self):
+        self.start_node = None
+        
+    def insert_in_emptylist(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+        else:
+            print("list is not empty")
+
+    def insert_at_end(self, data):
+        if self.start_node is None:
+            new_node = Node(data)
+            self.start_node = new_node
+            return
+        n = self.start_node
+        while n.nref is not None:
+            n = n.nref
+        new_node = Node(data)
+        n.nref = new_node
+        new_node.pref = n
+        
+    def traverse_list(self):
+        if self.start_node is None:
+            print("List has no element")
+            return
+        else:
+            n = self.start_node
+            while n is not None:
+                print(n.item , " ")
+                n = n.nref
+                
+    def reverse_linked_list(self):
+        if self.start_node is None:
+            print("The list has no element to delete")
+            return
+        p = self.start_node
+        q = p.nref
+        p.nref = None
+        p.pref = q
+        while q is not None:
+            q.pref = q.nref
+            q.nref = p
+            p = q
+            q = q.pref
+        self.start_node = p
+
+b = DoublyLinkedList()
+a = input().split()
+for i in range(len(a)):
+    a[i] = int(a[i])
+    b.insert_at_end(a[i])
+b.reverse_linked_list()
+b.traverse_list()
+
+
